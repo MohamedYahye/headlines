06fda41b984a0eacb8ef8559e76c5b7d7db0fecc
Unknown <alimoha72@gmail.com>
2016-12-16 11:21:53 +0100

    versie 1.7
    registerpage and login page still to be made. add a like button to all articles

--- register.php
+++ register.php
@@ -9,12 +9,73 @@
 <head>
 	<title>Registter</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/registerstyle.css">
+	<script type="text/javascript" src="assets/js/jquery-3.1.1.min.js"></script>
 </head>
 <body>
 
 
 	<div class="container">
+
+		<div class="form-container">
+			
+			<form class="form" method="POST" action="register.php" name="registerForm">
+				<label class="label" for="name">Name:</label><br /><br />
+				<input type="text" name="name" id="name"><br /><br />
+				<label class="label" for="username">Username:</label><br /> <br />
+				<input type="text" name="username" id="username"><br /><br />
+				<label class="label" for="email">Email:</label><br /><br />
+				<input type="email" name="email" id="email"><br /><br />
+				<label class="label" for="password">Password:</label><br /><br />
+				<input type="password" name="password" id="password"><br /><br />
+				<label class="label" for="repeat" >Repeat password:</label><br /><br />
+				<input type="password" name="repeat" id="repeat"><br /><br />
+
+				<input id='submit'type="submit" value="submit" name="submit">
+
+
+			</form>
+
+
+		</div>
+
 	</div>
+
+
+	<script type="text/javascript">
+		
+		$(function() {
+
+			var proceed = false;
+
+			var name = returnElement("name");
+			var username = returnElement("username");
+			var email = returnElement("email");
+			var password = returnElement("password");
+			var repeat = returnElement("repeat");
+			var submit = returnElement("submit");
+
+
+			
+
+
+			submit.on("click", function(){
+				
+			})
+
+
+			function returnElement($id){
+
+				return $("#" + $id);
+			}
+
+
+
+		});
+		
+
+
+		
+	</script>
 
 </body>
 </html>
